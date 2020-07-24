8f8d567496b45980b96859e22767f1426fd014f7
drumath <2237k.drumath@gmail.com>
2020-07-24 16:11:51 +0900

    :sparkles: テキトーにコンポーネントを作ってみた

---
+++ components/ARFaceViewer/ARFaceViewer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <facemesh-video v-show="isVisible"/>
+      <three-canvas v-show="isVisible"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FacemeshVideo from '~/components/ARFaceViewer/FacemeshVideo.vue'
+import ThreeCanvas from '~/components/ARFaceViewer/ThreeCanvas.vue'
+
+export default Vue.extend({
+  components: {
+    FacemeshVideo,
+    ThreeCanvas
+  },
+  data() {
+    return {
+      isVisible: false
+    }
+  }
+})
+</script>
