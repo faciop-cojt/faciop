2ecb189c83db0d5a573a573d74b678256d2ba5c6
drumath <2237k.drumath@gmail.com>
2020-07-24 15:34:59 +0900

    :sparkles: nuxt configへpluginの登録をした

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {src: '~/plugins/FacemeshInjectionManager.ts', mode: 'client'}
+  ],
   /*
    ** Nuxt.js dev-modules
    */
